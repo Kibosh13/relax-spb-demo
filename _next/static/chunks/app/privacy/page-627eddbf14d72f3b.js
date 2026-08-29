fe4c9f7b1411f7dbed38ba64d7eaaa7220795eaa
Kibosh13 <Kibosh13@users.noreply.github.com>
2026-08-29 17:56:58 +0300

    Fix privacy link for GitHub Pages (basePath)

--- _next/static/chunks/app/privacy/page-627eddbf14d72f3b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[385],{1342:function(){}},function(n){n.O(0,[60,697,744],function(){return n(n.s=1342)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/privacy/page-db5f2446729396b9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[385],{5574:function(n,e,u){Promise.resolve().then(u.t.bind(u,4970,23))}},function(n){n.O(0,[970,60,697,744],function(){return n(n.s=5574)}),_N_E=n.O()}]);
